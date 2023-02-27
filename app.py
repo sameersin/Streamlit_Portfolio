e6c58b825247e49f081baef4f3f0b93a775cb498
sameer shaik <116141065+sameersin@users.noreply.github.com>
2023-02-28 01:48:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title('Portfolio!:wave:')
+
+st.header(':red[_SHAIK SAMEER_]')
+st.subheader('_Aspiring Data scientist_')
+
+st.text('''
+A resilient engineer with expertise in statistical analysis and machine learning.\n
+Proficient in Python, R, SQL, and Tableau.\n
+Skilled in data cleaning, feature engineering, and model evaluation.\n
+Passionate about solving complex problems and eager to contribute to world.\n
+''')
+
+st.header('I invite you to connect me on :')
+st.write(':pushpin: [Likedin](https://www.linkedin.com/in/sameershaik78/)')
+st.write(':safety_pin: [Github](https://github.com/sameersin)')
